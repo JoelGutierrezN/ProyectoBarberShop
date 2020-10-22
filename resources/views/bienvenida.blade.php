77f5ba3c47d8130774d2d3a6a989678252928e5d
JoelGutierrezN <al221910321@gmail.com>
2020-10-21 23:11:07 -0600

    avance al diseño

---
+++ resources/views/bienvenida.blade.php
@@ -0,0 +1,63 @@
+@include('includes.header')
+
+<!--Cuerpo de la pagina-->
+<div class="card text-white f-negro mb-3">
+
+    <!--Header-->
+    <header>
+        @include('includes.navegacion')
+    </header>
+
+    <!--Carrusel-->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="{{ asset('img/prueba.png') }}" class="d-block w-100" alt="..." height="350px">
+            </div>
+            <div class="carousel-item">
+            <img src="{{ asset('img/prueba.png') }}" class="d-block w-100" alt="..." height="350px">
+            </div>
+            <div class="carousel-item">
+            <img src="{{ asset('img/prueba.png') }}" class="d-block w-100" alt="..." height="350px">
+            </div>
+            <div class="carousel-item">
+            <img src="{{ asset('img/prueba.png') }}" class="d-block w-100" alt="..." height="350px">
+            </div>
+            <div class="carousel-item">
+            <img src="{{ asset('img/prueba.png') }}" class="d-block w-100" alt="..." height="350px">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <!--Contenido-->
+    <div class="container">
+        <main role="main" class="flex-Shrink-0">
+            <div class="container contenedor-titulos">
+                <p class="Titulo">Studio F BarberShop</p>
+                <div class="contenedor-subtitulo container">
+                    <p class="Subtitulo">Lo mejor del pais</p>
+                </div>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores vel perferendis voluptates magnam provident. At illo expedita accusamus quam placeat blanditiis cumque odio atque praesentium, quibusdam delectus beatae hic!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores vel perferendis voluptates magnam provident. At illo expedita accusamus quam placeat blanditiis cumque odio atque praesentium, quibusdam delectus beatae hic!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores vel perferendis voluptates magnam provident. At illo expedita accusamus quam placeat blanditiis cumque odio atque praesentium, quibusdam delectus beatae hic!</p>
+        </main>
+    </div>
+</div>
+
+@include('includes.footer')
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--Icono-->
+        <link rel="icon" href="{!! asset('favicon.ico') !!}" type="image/x-icon">
+        <!--Estilos Boostrap-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <!--Normalice-->
+        <link rel="stylesheet" href="{{ asset('css/normalice.css') }}">
+        <!--Estilos Personales-->
+        <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+
+        <!--Fuentes del proyecto-->
+
+            <!--Titulo y Subtitulos-->
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
+            <!--Texto-->
+        <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@300&display=swap" rel="stylesheet">
+
+        <title>Studio F | Inicio</title>
+    </head>
+    <body>
