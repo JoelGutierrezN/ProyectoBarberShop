52c2bc82cd940bc172a013534332984ab5dd0728
JoelGutierrezN <al221910321@gmail.com>
2020-10-26 20:32:56 -0700

    merge Joel

--- resources/views/bienvenida.blade.php
+++ resources/views/bienvenida.blade.php
@@ -9,17 +9,9 @@
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img src="{{ asset('img/prueba.png') }}" class="d-block w-100" alt="..." height="350px">
-            </div>
-            <div class="carousel-item">
-            <img src="{{ asset('img/prueba.png') }}" class="d-block w-100" alt="..." height="350px">
-            </div>
-            <div class="carousel-item">
             <img src="{{ asset('img/prueba.png') }}" class="d-block w-100" alt="..." height="350px">
             </div>
             <div class="carousel-item">
